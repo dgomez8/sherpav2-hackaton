2fd6a69e5ec95deb264daefb1b338adbad68f973
David Felipe Gómez Rodríguez <53060149+dgomez8@users.noreply.github.com>
2024-09-13 11:11:07 -0500

    Initialize staging

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('TestComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should validate text', () => {
+    expect(typeof (component.title)).toBe('string')
+    expect(component.title).toEqual('template');
+  });
+});
